3f33b87128d23374faa0d532cf9ede5251dba6c4
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-12-18 19:58:28 -0200

    Initial commit

---
+++ src/main/java/com/joaoabrodrigues/correiostrack/CorreiosTrackApplication.java
@@ -0,0 +1,14 @@
+package com.joaoabrodrigues.correiostrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreiosTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreiosTrackApplication.class, args);
+	}
+
+}
+
