86e943c138e4e13aa26b8e03eecde194be0fc762
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-12-18 22:15:22 -0200

    remove unused import

--- src/main/java/com/joaoabrodrigues/correiostrack/service/CorreiosTrackingService.java
+++ src/main/java/com/joaoabrodrigues/correiostrack/service/CorreiosTrackingService.java
@@ -8,7 +8,6 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 @Service
